dc8b5c8cfc99aa9e37e98e9fdce7cdd595f95784
Andrew Edmonds <aedmonds@lbl.gov>
2019-08-23 14:01:23 -0500

    Add option to override cfg for input file and tree names, and output filename with command line arguments

--- Main/src/roofitter_main.cc
+++ Main/src/roofitter_main.cc
@@ -23,20 +23,29 @@ namespace roofitter {
     bool need_help;
     bool debug_cfg;
     std::string debug_cfg_filename;
+    std::string input_filename;
+    std::string input_treename;
+    std::string output_filename;
   };
 
   void PrintHelp() {
     std::cout << "Input Arguments:" << std::endl;
     std::cout << "\t-c, --config [cfg file]: input configuration file" << std::endl;
+    std::cout << "\t-i, --input [root file]: input ROOT file containing the tree (overrides anything in cfg file)" << std::endl;
+    std::cout << "\t-t, --tree [tree name]: tree name (inc. directory) in the input file (overrides anything in cfg file)" << std::endl;
+    std::cout << "\t-o, --output [root file]: output ROOT file that will be created (overrides anything in cfg file)" << std::endl;
     std::cout << "\t-d, --debug-config [filename]: print out the final config file to file" << std::endl;
     std::cout << "\t-h, --help: print this help message" << std::endl;
   }
 
   void ProcessArgs(int argc, char** argv, InputArgs& args) {
-    const char* const short_opts = "c:d:h";
+    const char* const short_opts = "c:i:t:o:d:h";
 
     const option long_opts[] = {
       {"config", required_argument, nullptr, 'c'},
+      {"input", required_argument, nullptr, 'i'},
+      {"tree", required_argument, nullptr, 't'},
+      {"output", required_argument, nullptr, 'o'},
       {"debug-config", required_argument, nullptr, 'd'},
       {"help", no_argument, nullptr, 'h'},
       {nullptr, no_argument, nullptr, 0}
@@ -52,6 +61,18 @@ namespace roofitter {
     
       case 'c':
 	args.cfg_filename = std::string(optarg);
+	break;
+
+      case 'i':
+	args.input_filename = std::string(optarg);
+	break;
+
+      case 't':
+	args.input_treename = std::string(optarg);
+	break;
+
+      case 'o':
+	args.output_filename = std::string(optarg);
 	break;
 
       case 'd':
@@ -86,13 +107,25 @@ namespace roofitter {
     }
 
 
-    std::string filename = config.getString("input.filename");
+    std::string filename = config.getString("input.filename", "");
+    if (!args.input_filename.empty()) { // override cfg file with
+      filename = args.input_filename;
+    }
+    if (filename.empty()) {
+      throw cet::exception("roofitter::main()") << "No filename specified";
+    }
     TFile* file = new TFile(filename.c_str(), "READ");
     if (file->IsZombie()) {
       throw cet::exception("roofitter::main") << "Input file " << filename << " is a zombie";
     }
 
-    std::string treename = config.getString("input.treename");
+    std::string treename = config.getString("input.treename", "");
+    if (!args.input_treename.empty()) { // override cfg tree with
+      treename = args.input_treename;
+    }
+    if (treename.empty()) {
+      throw cet::exception("roofitter::main()") << "No treename specified";
+    }
     TTree* tree = (TTree*) file->Get(treename.c_str());
     if (!tree) {
       throw cet::exception("roofitter::main") << "Input tree " << treename << " is not in file";
@@ -114,7 +147,13 @@ namespace roofitter {
       i_ana.calculate();
     }
 
-    std::string outfilename = config.getString("output.filename");
+    std::string outfilename = config.getString("output.filename", "");
+    if (!args.output_filename.empty()) { // override cfg file with
+      outfilename = args.output_filename;
+    }
+    if (outfilename.empty()) {
+      throw cet::exception("roofitter::main()") << "No outfilename specified";
+    }
     TFile* outfile = new TFile(outfilename.c_str(), "RECREATE");
     for (auto& i_ana : analyses) {
       TDirectory* outdir = outfile->mkdir(i_ana.name.c_str());
